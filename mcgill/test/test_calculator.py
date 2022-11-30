7e6f89cf2b9ab3fcb22856da4b24f54db8d17e39
daveps99 <david.pelland-sauve@mail.mcgill.ca>
2022-11-30 10:56:49 -0500

    build fail works, remove useless test

--- mcgill/test/test_calculator.py
+++ mcgill/test/test_calculator.py
@@ -10,9 +10,3 @@ class CalculatorTest(unittest.TestCase):
         result = self.calc.division(10,5)
 
         self.assertEqual(result, self.calc.answer, "incorrect")
-
-    #this should fail the build since addition is not implemented
-    def test_addition(self):
-        result = self.calc.addition(10,5)
-
-        self.assertEqual(result, self.calc.answer, "incorrect")
