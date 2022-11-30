6b4f152f54d757fd9833d97decac982407400b0a
daveps99 <david.pelland-sauve@mail.mcgill.ca>
2022-11-30 11:12:16 -0500

    added functions to calc app

--- mcgill/src/calculator.py
+++ mcgill/src/calculator.py
@@ -1,8 +1,23 @@
 
-# Sample python script for CI setup
+
 class Calculator(object):
+    '''
+    Calculator app used for statistical computations of the student grades.
+    '''
     def __init__(self):
         self.answer = 0
+
+    def addition(self,a,b):
+        self.answer = a+b
+        return self.answer
+
+    def substraction(self,a,b):
+        self.answer = a-b
+        return self.answer
+
+    def multiplication(self,a,b):
+        self.answer = a*b
+        return self.answer
 
     def division(self,a,b):
         self.answer = a/b
